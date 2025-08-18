5bdc70bcac13607b9786daa0531a2c8e6359c3fb
ThomasAhSing <ahsingthomas@gmail.com>
2025-08-18 10:41:48 -0700

    Config changes

--- app.config.js
+++ app.config.js
@@ -1,11 +1,71 @@
 import 'dotenv/config';
 
-export default {
-  expo: {
-    name: "UBCFirstYearLifeApp",
-    slug: "ubcfirstyearlifeapp",
-    extra: {
-      ADMIN_PASSCODE: process.env.ADMIN_PASSCODE,
+export default ({ config }) => ({
+  ...config,
+  owner: "ahsingthomas",
+  name: "FirstYearLife",
+  slug: "firstyearlife",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/ubcfyla_app_icon.png",
+  scheme: "firstyearlife",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: "com.ahsingthomas.firstyearlife",
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false
     }
+  },
+
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/ubcfyla_app_icon.png",
+      backgroundColor: "#0C2A42"
+    },
+    edgeToEdgeEnabled: true,
+    package: "com.ahsingthomas.firstyearlife"
+  },
+
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/ubcfyla_app_icon.png"
+  },
+
+  plugins: [
+    "expo-notifications",
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/ubcfyla_splashscreen.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#0C2A42"
+      }
+    ],
+    [
+      "expo-image-picker",
+      {
+        photosPermission:
+          "The app accesses photos to let admin upload photos for posts. ADMIN USE ONLY. Permission to access photos will not be asked to user"
+      }
+    ],
+    "expo-font",
+    "expo-web-browser"
+  ],
+
+  experiments: {
+    typedRoutes: true
+  },
+
+  extra: {
+    eas: {
+      projectId: "a4c5d400-288e-447f-9133-7065f74f1ef6"
+    },
+    ADMIN_PASSCODE: process.env.ADMIN_PASSCODE
   }
-};
+});
