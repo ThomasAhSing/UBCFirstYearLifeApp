1abd8757df69b86fc5c958c4ad693ee37752efe5
ThomasAhSing <ahsingthomas@gmail.com>
2025-08-21 02:06:33 -0700

    Updated build number

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ export default ({ config }) => ({
   ios: {
     supportsTablet: false,
     bundleIdentifier: "com.ahsingthomas.firstyearlife",
-    buildNumber: "3",
+    buildNumber: "4",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false
     }
